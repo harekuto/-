26a6496df2352fc17ba2c624756ce1e6c7b55e49
harekuto <hillerjojojo@gmail.com>
2026-09-02 11:29:43 -0400

    Clean per-player relay state on logout

--- voxline-0.7.0/src/main/java/com/voxline/voice/server/ServerEvents.java
+++ voxline-0.7.0/src/main/java/com/voxline/voice/server/ServerEvents.java
@@ -9,14 +9,33 @@ import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public final class ServerEvents {
-    @SubscribeEvent public void onServerStart(ServerAboutToStartEvent event) { ServerConfig.INSTANCE.load(); GroupManager.clear(); VoiceRelay.clear(); }
-    @SubscribeEvent public void onLogin(PlayerEvent.PlayerLoggedInEvent event) {
+    @SubscribeEvent
+    public void onServerStart(ServerAboutToStartEvent event) {
+        ServerConfig.INSTANCE.load();
+        GroupManager.clear();
+        VoiceRelay.clear();
+    }
+
+    @SubscribeEvent
+    public void onLogin(PlayerEvent.PlayerLoggedInEvent event) {
         if (event.getEntity() instanceof ServerPlayer p) {
             ServerConfig c = ServerConfig.INSTANCE;
-            VoiceNetwork.sendTo(p, new ServerPolicyS2CPacket(c.enabled, c.groupsEnabled, (float)c.maxRange, c.maxGroupSize));
+            VoiceNetwork.sendTo(p, new ServerPolicyS2CPacket(c.enabled, c.groupsEnabled, (float) c.maxRange, c.maxGroupSize));
             GroupManager.sendState(p);
         }
     }
-    @SubscribeEvent public void onLogout(PlayerEvent.PlayerLoggedOutEvent event) { if (event.getEntity() instanceof ServerPlayer p) GroupManager.onLogout(p); }
-    @SubscribeEvent public void onStop(ServerStoppingEvent event) { GroupManager.clear(); VoiceRelay.clear(); }
+
+    @SubscribeEvent
+    public void onLogout(PlayerEvent.PlayerLoggedOutEvent event) {
+        if (event.getEntity() instanceof ServerPlayer p) {
+            VoiceRelay.clear(p.getUUID());
+            GroupManager.onLogout(p);
+        }
+    }
+
+    @SubscribeEvent
+    public void onStop(ServerStoppingEvent event) {
+        GroupManager.clear();
+        VoiceRelay.clear();
+    }
 }
